3331b1ffc8f56b9c63cf2ecf6cec75deca32033a
icecreameyyy <21210024@unima.ac.id>
2026-03-27 20:52:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ st.markdown(
     """
     <style>
         .judul-responsif {
-            font-size: 55px; /* Ukuran di Laptop */
+            font-size: 50px; /* Ukuran di Laptop */
             font-weight: bold;
             line-height: 1.2;
             text-align: left;
@@ -101,7 +101,7 @@ st.markdown(
         /* Kode khusus untuk layar kecil (HP) */
         @media (max-width: 600px) {
             .judul-responsif {
-                font-size: 37px; /* Huruf mengecil sedikit di HP agar pas 2-3 baris */
+                font-size: 38px; /* Huruf mengecil sedikit di HP agar pas 2-3 baris */
             }
         }
     </style>
